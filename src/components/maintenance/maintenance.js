2cdde068e1baeac2cadba9a10bdaa44b87f8156d
jmdelrosario1623 <jm.delrosario@systemoph.com>
2020-01-28 16:32:17 +0800

    updated css

--- src/components/maintenance/maintenance.js
+++ src/components/maintenance/maintenance.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 import './style.css';
 import './mobile.css';
 
+import LogoMaintenance from './images/logo.png';
+import MaintenanceImg from './images/maintenance.png';
 
 export default class Maintenance extends Component {
     render() {
@@ -10,12 +12,12 @@ export default class Maintenance extends Component {
             <section id="maintenance">
                 <header>
                     <a href="https://systemoph.com/">
-                    <img src="Images/logo.png" alt="" />
+                    <img src={LogoMaintenance} alt="" />
                     </a>
                 </header>
                 <main class="showcase container">
                     <div>
-                    <img src="Images/maintenance.png" alt="" />
+                    <img src={MaintenanceImg} alt="" />
                     </div>
                     <div class="lead">
                     <h1>The site is currently down for maintenance.</h1>
