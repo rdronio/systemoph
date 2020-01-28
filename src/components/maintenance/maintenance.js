ba440d2b58f00ea4c20272a462691c4504a141cd
jmdelrosario1623 <jm.delrosario@systemoph.com>
2020-01-28 16:30:34 +0800

    updated css

---
+++ src/components/maintenance/maintenance.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+import './style.css';
+import './mobile.css';
+
+
+export default class Maintenance extends Component {
+    render() {
+        return (
+            <section id="maintenance">
+                <header>
+                    <a href="https://systemoph.com/">
+                    <img src="Images/logo.png" alt="" />
+                    </a>
+                </header>
+                <main class="showcase container">
+                    <div>
+                    <img src="Images/maintenance.png" alt="" />
+                    </div>
+                    <div class="lead">
+                    <h1>The site is currently down for maintenance.</h1>
+                    <p>
+                        Our website is undergoing scheduled maintenance, we will be right
+                        back soon.<br />Thank you for your patience.
+                    </p>
+                    </div>
+                </main>
+                <footer>
+                    <h4>Follow Us!</h4>
+                    <a href="https://www.facebook.com/systemoph/"
+                    ><i class="fab fa-facebook-square"></i
+                    ></a>
+                    <a href=""><i class="fab fa-twitter-square"></i></a>
+                </footer>
+            </section>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import App from './App';
+import Maintenance from './components/maintenance/maintenance';
+
 import * as serviceWorker from './serviceWorker';
 
 import 'semantic-ui-css/semantic.min.css'
 import "animate.css/animate.min.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Maintenance />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
