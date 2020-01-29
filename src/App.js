345d87ee2cba751d1c815248b638c4bf46ce8233
jmdelrosario1623 <jm.delrosario@systemoph.com>
2020-01-29 08:31:32 +0800

    update body

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-import Menu from './components/menu/menu';
-import About from './components/about/about';
-import Service from './components/services/services';
-import Careers from './components/careers/careers';
-import Projects from './components/projects/projects';
-import Footer from './components/footer/footer';
+// import Menu from './components/menu/menu';
+// import About from './components/about/about';
+// import Service from './components/services/services';
+// import Careers from './components/careers/careers';
+// import Projects from './components/projects/projects';
+// import Footer from './components/footer/footer';
+
+import Maintenance from './components/maintenance/maintenance';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
   return (
     <div>
-      <Menu />
+      {/* <Menu />
       <Service />
       <About />
       <Careers />
@@ -22,7 +24,12 @@ function App() {
       <MessengerCustomerChat
         pageId=" 106849417483586"
         appId="2370605603251620"
-      />
+      /> */}
+      <Maintenance />
+      <MessengerCustomerChat
+        pageId=" 106849417483586"
+        appId="2370605603251620"
+      /> 
     </div>
   );
 }
