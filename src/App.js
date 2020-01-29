0d76f5017ef44e012512d2faac23145be01e3a6a
jmdelrosario1623 <jm.delrosario@systemoph.com>
2020-01-29 09:17:36 +0800

    update messenger

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-import Menu from './components/menu/menu';
-import About from './components/about/about';
-import Service from './components/services/services';
-import Careers from './components/careers/careers';
-import Projects from './components/projects/projects';
-import Footer from './components/footer/footer';
+// import Menu from './components/menu/menu';
+// import About from './components/about/about';
+// import Service from './components/services/services';
+// import Careers from './components/careers/careers';
+// import Projects from './components/projects/projects';
+// import Footer from './components/footer/footer';
 
-// import Maintenance from './components/maintenance/maintenance';
+import Maintenance from './components/maintenance/maintenance';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
@@ -25,11 +25,11 @@ function App() {
         pageId="106849417483586"
         appId="2370605603251620"
       /> */}
-      {/* <Maintenance />
-      // <MessengerCustomerChat
-      //   pageId="105340060897659"
-      //   appId="2370605603251620"
-      // />  */}
+      <Maintenance />
+      <MessengerCustomerChat
+        pageId="105340060897659"
+        appId="2370605603251620"
+      /> 
     </div>
   );
 }
