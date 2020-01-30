121e46ade55cd6782c1d7afc6eb4b627d7a92d4a
jmdelrosario1623 <jm.delrosario@systemoph.com>
2020-01-30 08:36:02 +0800

    first commit

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -12,18 +12,18 @@ export default class About extends Component {
                 backgroundImage:  `url(${bg_footer})`}} >
                 <div className="footer-content">
                     <Grid stackable columns={3}>
-                        <Grid.Column width="5" className="">
+                        <Grid.Column width="4" className="">
                             <Image src={logo} size="medium" />
                         </Grid.Column>
 
-                        <Grid.Column width="5">
+                        <Grid.Column width="4">
                             <div className="footer-contact-us"> Contact Details </div>
                             <div className="footer-contact-us-content">
                                 <p> info@systemoph.com <br /> +639175553844 <br /> +639612452478 <br /> +639190002586</p>
                             </div>
                         </Grid.Column>
 
-                        <Grid.Column width="5" className="">
+                        <Grid.Column width="4" className="address">
                             <div className="footer-address"> Address </div>
                             <div className="footer-address-content">
                                 Main: 776 St. Dominic Subd., San Miguel Betis, Guagua, Pampanga 2003    
@@ -33,16 +33,15 @@ export default class About extends Component {
                             </div>
                         </Grid.Column>
 
-                        {/* <Grid.Column width="4" className="">
-                            <div className="footer-hours"> Office Hours </div>
+                        <Grid.Column width="4" className="social">
+                            <div className="footer-hours"> Follow us! </div>
                             <div className="footer-hours-content">
-                                Mon:09:00 am – 05:00 pm <br />
-                                Tue:09:00 am – 05:00 pm <br />
-                                Wed:09:00 am – 05:00 pm <br /> 
-                                Thu:09:00 am – 05:00 pm <br />
-                                Fri:09:00 am – 05:00 pm <br />
+                                <span> </span>
+                                <span> </span>
+                                <span> </span>
+                                <span> </span>
                             </div>
-                        </Grid.Column> */}
+                        </Grid.Column>
                     </Grid>
                 </div>
             </Segment>
