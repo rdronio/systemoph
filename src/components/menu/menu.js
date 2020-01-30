f9abf01f097186da78642bb07a248ac9d172d236
jmdelrosario1623 <jm.delrosario@systemoph.com>
2020-01-30 09:29:32 +0800

    first commit

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -31,7 +31,7 @@ export default class MenuHeader extends Component {
                                 onClick={this.handleItemClick}
                             />
                             <Menu.Item
-                                as={ Link }  to='.about' name='about'
+                                as={ Link }  to='#about' name='about'
                             />
                             <Menu.Item
                                 name='careers'
