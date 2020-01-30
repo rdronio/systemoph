777895a55b84fa98d978275924db0380152248eb
jmdelrosario1623 <jm.delrosario@systemoph.com>
2020-01-30 10:55:49 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-// import * as Scroll from 'react-scroll';
-// import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 import './App.css';
@@ -17,27 +14,6 @@ import Projects from './components/projects/projects';
 import Footer from './components/footer/footer';
 
 import Maintenance from './components/maintenance/maintenance';
-
-// export default class App extends React.Component {
-//   render() {
-//       return (
-//         <div>
-//           <Menu />
-//           <Service />
-//           <SlickServices />
-//           <About />
-//           <Careers />
-//           <Projects />
-//           <Footer />
-//           <Maintenance />
-//           <MessengerCustomerChat
-//             pageId="105340060897659"
-//             appId="2370605603251620"
-//           /> 
-//         </div>
-//       )
-//   }
-// }
 
 function App() {
   
