26651d71efa7600e9c6839385c6b6ae307210038
jmdelrosario1623 <jm.delrosario@systemoph.com>
2020-01-30 11:51:43 +0800

    first commit

--- src/components/maintenance/maintenance.js
+++ src/components/maintenance/maintenance.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
 import { Segment } from 'semantic-ui-react'
 
-import './style.css';
-import './mobile.css';
+// import './style.css';
+// import './mobile.css';
 
-import LogoMaintenance from './images/logo.png';
-import MaintenanceImg from './images/maintenance.png';
+// import LogoMaintenance from './images/logo.png';
+// import MaintenanceImg from './images/maintenance.png';
 
 export default class Maintenance extends Component {
     render() {
         return (
-            <Segment className="maintenance-bottom" vertical textAlign='center' >
+            <Segment className="maintenance-bottom" vertical textAlign='center'>
                 Our site is under development, please bear with us. 
             </Segment>
         )
